4948f73c3187c71f4401e87003fc89d22af2212c
marchen12 <130596644+marchen12@users.noreply.github.com>
2023-05-06 07:34:15 +0800

    ok,run

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-
-Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-
