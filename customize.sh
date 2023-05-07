d416bac35284cfca7f6d91e4e7bc9238e282668a
marchen12 <130596644+marchen12@users.noreply.github.com>
2023-05-07 22:37:04 +0800

    OK,run!

--- customize.sh
+++ customize.sh
@@ -11,5 +11,5 @@ sed -i 's/192.168.1.1/192.168.11.1/g' openwrt/package/base-files/files/bin/confi
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-3. Replace with JerryKuKu’s Argon
-rm openwrt/package/lean/luci-theme-argon -rf
+#3. Replace with JerryKuKu’s Argon
+#rm openwrt/package/lean/luci-theme-argon -rf
